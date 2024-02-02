64a5d38c8a74bc0106300bc33863fc7a8c414861
teraiken <terai.ken@relation2012.com>
2024-02-02 22:40:09 +0900

    section6

--- client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 import Head from 'next/head'
 import React from 'react'
 
-const Detail = ({ detail }) => {
+const Detail = ({ detail, media_type }) => {
     return (
         <AppLayout
             header={
@@ -62,11 +62,13 @@ const Detail = ({ detail }) => {
 
                         <Grid item md={8}>
                             <Typography variant="h4" paragraph>
-                                {detail.title}
+                                {detail.title || detail.name}
                             </Typography>
                             <Typography paragraph>{detail.overview}</Typography>
                             <Typography variant="h6">
-                                公開日:{detail.release_date}
+                                {media_type == 'movie'
+                                    ? `公開日:${detail.release_date}`
+                                    : `初回放送日:${detail.first_air_date}`}
                             </Typography>
                         </Grid>
                     </Grid>
@@ -95,7 +97,7 @@ export async function getServerSideProps(context) {
         }
 
         return {
-            props: { detail: combinedData },
+            props: { detail: combinedData, media_type },
         }
     } catch {
         return { notFound: true }
